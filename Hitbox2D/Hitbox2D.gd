5c384ee20422ce5259ae201e7c052551ee572046
that-damn-cat <saihenjin@gmail.com>
2025-11-25 18:28:52 -0600

    Finished Hitbox2D
    Finished Documentation, removed excessive functions (unneeded setter, unused function)

--- Hitbox2D/Hitbox2D.gd
+++ Hitbox2D/Hitbox2D.gd
@@ -1,9 +1,14 @@
+## Hitbox implementation of an Area2D. If it collides with a Hurtbox2D
+## it will deal damage to it.
+
 @icon("./punch-blast.svg")
 class_name Hitbox2D
 extends Area2D
 
+## Emitted when a collision with a Hurtbox2D is detected
 signal hit_target
 
+## Amount of damage to deal to collided Hurtbox2D
 @export var damage: int
 
 
@@ -11,19 +16,6 @@ func _ready() -> void:
 	area_entered.connect(_on_hit)
 
 
-#where to change the amount of damage a thing has done
-func set_damage(value: int) -> void:
-	damage = value
-
-
-#damage multipler or divider
-func get_scaled_damage(factor: float) -> int:
-	var result: int = (damage * factor) as int
-
-	return (result)
-
-
-#did i hit something? if its a hurtbox E M I T
 func _on_hit(area: Area2D) -> void:
 	if area is Hurtbox2D:
 		hit_target.emit()
