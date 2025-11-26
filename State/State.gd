e2f230e587817352f4829630ee91a056dd574f55
that-damn-cat <saihenjin@gmail.com>
2025-11-25 18:43:50 -0600

    Documented State

--- State/State.gd
+++ State/State.gd
@@ -1,12 +1,18 @@
+## A node based State for the StateMachine
+
 @icon("./choice.svg")
 @tool
 class_name State
 extends Node
 
+## Emits when this state is requesting a transition to a new state.
 signal transitioned(this_state: State, new_state_name: String)
-var state_machine : StateMachine
 
+## The state name used to find this state in the StateMachine
 @export var state_name: String = name.to_lower()
+
+var _state_machine : StateMachine
+
 
 func _get_configuration_warnings() -> PackedStringArray:
 	var warnings = []
@@ -14,6 +20,7 @@ func _get_configuration_warnings() -> PackedStringArray:
 		warnings.append("State nodes must be children of a StateMachine node.")
 
 	return warnings
+
 
 func _notification(what: int) -> void:
 	var triggers := [
@@ -29,18 +36,32 @@ func _notification(what: int) -> void:
 	if Engine.is_editor_hint() and what in triggers:
 		update_configuration_warnings()
 
+
+## To be implemented by the inheriting node. Called when the state is first entered.
 func enter() -> void:
 	pass
 
+
+## To be implemented by the inheriting node. Called when the state is exited.
 func exit() -> void:
 	pass
 
+
+## To be implemented by the inheriting node. Called with _process
 func update(_delta: float) -> void:
 	pass
 
+
+## To be implemented by the inheriting node. Called with _physics_process
 func physics_update(_delta: float) -> void:
 	pass
 
+
+## Emit the transitioned signal
+func transition(new_state_name: String) -> void:
+	transitioned.emit(self, new_state_name)
+
+
 func _exit_tree() -> void:
-	if state_machine:
-		state_machine.remove_state(self)
+	if _state_machine:
+		_state_machine.remove_state(self)
