5a6cf7d0add4376b151667ec2421a9b8a30273b7
that-damn-cat <saihenjin@gmail.com>
2025-11-30 17:57:32 -0600

    Removed tool annotation from StateMachine
    Proved to be more complexity than necessary.

--- State/State.gd
+++ State/State.gd
@@ -1,7 +1,6 @@
 ## A node based State for the StateMachine
 
 @icon("./choice.svg")
-@tool
 class_name State
 extends Node
 
@@ -11,31 +10,12 @@ signal transitioned(this_state: State, new_state_name: String)
 ## The state name used to find this state in the StateMachine
 @export var state_name: String = name.to_lower()
 
-var _state_machine : StateMachine
+var state_machine: StateMachine
 
 
-func _get_configuration_warnings() -> PackedStringArray:
-	var warnings = []
+func _ready() -> void:
 	if get_parent() is not StateMachine:
-		warnings.append("State nodes must be children of a StateMachine node.")
-
-	return warnings
-
-
-func _notification(what: int) -> void:
-	var triggers := [
-			NOTIFICATION_READY,
-			NOTIFICATION_CHILD_ORDER_CHANGED,
-			NOTIFICATION_EDITOR_POST_SAVE,
-	]
-
-	if Engine.is_editor_hint():
-		if state_name == "":
-			state_name = name.to_lower()
-			notify_property_list_changed()
-
-		if what in triggers:
-			update_configuration_warnings()
+		push_error("State nodes must be children of a StateMachine Node!")
 
 
 ## To be implemented by the inheriting node. Called when the state is first entered.
@@ -64,5 +44,5 @@ func transition_to(new_state_name: String) -> void:
 
 
 func _exit_tree() -> void:
-	if _state_machine:
-		_state_machine.remove_state(self)
+	if state_machine:
+		state_machine.remove_state(self)
--- StateMachine/StateMachine.gd
+++ StateMachine/StateMachine.gd
@@ -1,6 +1,4 @@
 ## A Node based state machine that manages State children and handles transitions
-
-@tool
 @icon("./gears.svg")
 class_name StateMachine
 extends Node
@@ -22,9 +20,6 @@ var previous_state: State
 
 
 func _enter_tree() -> void:
-	if Engine.is_editor_hint():
-		return
-
 	states = { }
 
 	for child in get_children():
@@ -36,39 +31,38 @@ func _ready() -> void:
 	if initial_state:
 		_set_state(initial_state)
 
+	if states.size() == 0:
+		push_warning("StateMachine has no State children at ready!")
+
 
 func _process(delta: float) -> void:
-	if Engine.is_editor_hint():
-		return
-
 	if current_state:
 		current_state.update(delta)
 
 
 func _physics_process(delta: float) -> void:
-	if Engine.is_editor_hint():
-		return
-
 	if current_state:
 		current_state.physics_update(delta)
 
 
 ## Returns the state object with the given name
-func get_state(state_name : String) -> State:
+func get_state(state_name: String) -> State:
 	return(states.get(state_name))
 
 
 ## Adds a given state to the State Machine
-func add_state(new_state : State) -> void:
+func add_state(new_state: State) -> void:
 	new_state.state_machine = self
 
-	# The new state must be a direct child of the StateMachine.
-	if new_state.get_parent() != self:
-		new_state.get_parent().remove_child(new_state)
-		add_child(new_state)
+	# The new state must be a direct child of the StateMachine. Put it there if it isn't.
+	if not new_state.get_parent() == self:
+		# Defer calls to end of frame in case reparenting is already happening from somewhere else
+		new_state.get_parent().call_deferred("remove_child", new_state)
+		self.call_deferred("add_child", new_state)
 
 	if states.has(new_state.state_name):
-		push_warning("Duplicate state_name '%s' detected." % new_state.state_name)
+		push_warning("Duplicate state_name '%s' detected. State being overwritten!" % new_state.state_name)
+		remove_state(new_state)
 
 	states[new_state.state_name] = new_state
 	new_state.transitioned.connect(_on_state_transitioned)
@@ -79,6 +73,7 @@ func add_state(new_state : State) -> void:
 ## Finally, if there is no initial state, the StateMachine will have no current state.
 func remove_state(state_to_remove: State) -> void:
 	if not states.has(state_to_remove.state_name):
+		push_warning("Error trying to remove state '%s', state not in dictionary!" % state_to_remove.state_name)
 		return
 
 	# Remove references to the removed state in previous/initial
@@ -97,6 +92,7 @@ func remove_state(state_to_remove: State) -> void:
 		else:
 			current_state = null
 
+	# Remove it from the dictionary and disconnect its transition signal
 	states.erase(state_to_remove.state_name)
 	state_to_remove.transitioned.disconnect(_on_state_transitioned)
 
@@ -108,7 +104,7 @@ func has_state(state_name: String) -> bool:
 
 ## Forces a state change to the given state name.
 func change_state(new_state_name: String) -> void:
-	var new_state := get_state(new_state_name)
+	var new_state: State = get_state(new_state_name)
 
 	if not new_state:
 		push_error("State '%s' does not exist in the state machine." % new_state_name)
@@ -116,51 +112,10 @@ func change_state(new_state_name: String) -> void:
 
 	_set_state(new_state)
 
-func _notification(what: int) -> void:
-	var triggers := [
-		NOTIFICATION_READY,
-		NOTIFICATION_CHILD_ORDER_CHANGED,
-		NOTIFICATION_EDITOR_POST_SAVE,
-	]
-
-	if Engine.is_editor_hint() and what in triggers:
-		update_configuration_warnings()
-
-
-func _get_configuration_warnings() -> PackedStringArray:
-	var warnings = []
-
-	var state_children: Array[State] = []
-	for child in get_children():
-		if child is State:
-			state_children.append(child)
-
-	if state_children.size() == 0:
-		warnings.append("StateMachine has no State children.")
-
-	var state_names := {}
-	var duplicates := ""
-
-	for state in state_children:
-		var this_name := state.state_name
-
-		if state_names.has(this_name):
-			duplicates += "- '%s'\n" % this_name
-		else:
-			state_names[this_name] = true
-
-	if not duplicates == "":
-		warnings.append("Duplicate state_name values detected:\n" + duplicates)
-
-	return warnings
-
 
 func _on_state_transitioned(this_state: State, new_state_name: String) -> void:
-	if Engine.is_editor_hint():
-		return
-
 	if this_state != current_state:
-		push_error("State transition signal received from a state that is not the current state.")
+		push_warning("State transition signal received from a state '%s' which is not the current state!" % this_state.state_name)
 		return
 
 	var new_state: State = get_state(new_state_name)
