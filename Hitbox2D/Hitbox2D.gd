c637c40638ea1300f53f33b98680333abd4e92a3
that-damn-cat <saihenjin@gmail.com>
2025-12-03 23:27:36 -0600

    Healthcomponent Handling Cleanup, Signal update

--- Hitbox2D/Hitbox2D.gd
+++ Hitbox2D/Hitbox2D.gd
@@ -6,7 +6,7 @@ class_name Hitbox2D
 extends Area2D
 
 ## Emitted when a collision with a Hurtbox2D is detected
-signal hit_target
+signal hit_hurtbox(hurtbox: Hurtbox2D)
 
 ## Amount of damage to deal to collided Hurtbox2D
 @export var damage: int
@@ -18,4 +18,4 @@ func _ready() -> void:
 
 func _on_hit(area: Area2D) -> void:
 	if area is Hurtbox2D:
-		hit_target.emit()
+		hit_hurtbox.emit(area as Hurtbox2D)
--- Hurtbox2D/Hurtbox2D.gd
+++ Hurtbox2D/Hurtbox2D.gd
@@ -9,15 +9,10 @@ extends Area2D
 ## HealthComponent that accepts damage
 @export var health: HealthComponent
 
-
 func _ready() -> void:
 	area_entered.connect(_on_hit)
 
 
 func _on_hit(area: Area2D) -> void:
 	if area is Hitbox2D:
-		# We can use negative "damage" to heal
-		if area.damage < 0:
-			health.heal(area.damage)
-		else:
-			health.damage(area.damage)
+		health.damage(area.damage)
